91c9c064bb0c56b48233298c0c229059cf6509f4
amitsingh011194 <astallvibes@gmail.com>
2025-07-03 18:16:55 +0530

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -1,24 +1,21 @@
-# ------------------- ADD THIS FOR SELENIUM WEBDRIVER -------------------
-
-from selenium.webdriver.chrome.service import Service as ChromeService
+from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from webdriver_manager.chrome import ChromeDriverManager
-
-@pytest.fixture
-def driver():
-    """Provide a Selenium WebDriver instance using Chrome."""
-    chrome_options = Options()
-    chrome_options.add_argument("--headless")  # Use GUI browser in local by commenting this line
-    chrome_options.add_argument("--no-sandbox")
-    chrome_options.add_argument("--disable-dev-shm-usage")
-    chrome_options.add_argument("--window-size=1920,1080")
-
-    service = ChromeService(ChromeDriverManager().install())
-    driver = webdriver.Chrome(service=service, options=chrome_options)
-    yield driver
-    driver.quit()
-
-
-@pytest.fixture
-def main_page(driver):
-    return MainPage(driver)
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+ 
+def __init__(self, headless=True):
+chrome_options = Options()
+if headless:
+chrome_options.add_argument("--headless")
+chrome_options.add_argument("--no-sandbox")
+chrome_options.add_argument("--disable-dev-shm-usage")
+chrome_options.add_argument("--disable-gpu")
+chrome_options.add_argument("--window-size=1920,1080")
+ 
+self.driver = webdriver.Chrome(options=chrome_options)
+self.wait = WebDriverWait(self.driver, 10)
+ 
+def open(self, url):
+self.driver.get(url)
+ 
