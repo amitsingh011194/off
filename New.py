beab3423e7b4ae3d12dbee6c94798eb3323032b7
amitsingh011194 <astallvibes@gmail.com>
2026-02-17 17:47:19 +0530

    Update New.py

--- New.py
+++ New.py
@@ -1,209 +1,144 @@
-
-# fnbo and af s3 triggers go here
-resource "aws_s3_bucket_notification" "skeps_gatekeeper_bucket_fnbo" {
-  bucket = "${var.customer_id}-tops-${var.environment}-s3-dps"
-
-  count = var.customer_id == "fnbo" ? 1 : 0
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-gatekeeper"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/fnbo/etl/original-files/"
-  }
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-etl"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/fnbo/etl/manageable-files/etl/"
-  }
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-datachecks"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/fnbo/etl/manageable-files/datacheck/"
-  }
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-selective-data-loader"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/fnbo/etl/process-start/"
-  }
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-file-merger"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/fnbo/etl/manageable-files/file-merger/"
-  } 
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-gatekeeper"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/af/etl/original-files/"
-  }
-  
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-etl"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/af/etl/manageable-files/etl/"
-  }
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-datachecks"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/af/etl/manageable-files/datacheck/"
-  }
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-selective-data-loader"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/af/etl/process-start/"
-  }
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-file-merger"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/af/etl/manageable-files/file-merger/"
-  }
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-af_forwardflow_savefile"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/af/legacy/forward-flow/"
-  }
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-af-paper-statement"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/af/reports/paper_statement/ps_start/"
-  }
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-gatekeeper"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/ucl/etl/original-files/"
-  }
-  
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-etl"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/ucl/etl/manageable-files/etl/"
-  }
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-datachecks"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/ucl/etl/manageable-files/datacheck/"
-  }
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-selective-data-loader"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/ucl/etl/process-start/"
-  }
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-file-merger"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/ucl/etl/manageable-files/file-merger/"
-  }
-
-  lambda_function {
-     lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:pos-techops-serverless-${var.environment}-agencyPostingHandler"
-     events              = ["s3:ObjectCreated:*"]
-     filter_prefix       = "clients/fnbo/agencyFiles/process_start"
-     filter_suffix       = ".csv"
-  }
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:pos-techops-serverless-${var.environment}-agencyPostingHandler"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/af/agencyFiles/process_start"
-    filter_suffix       = ".csv"
-  }
-
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:pos-techops-serverless-${var.environment}-agencyPostingHandler"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/ucl/agencyFiles/process_start"
-    filter_suffix       = ".csv"
-  }
-
-  depends_on = [
-    module.deploy_lambdas,
-    aws_lambda_permission.lambda_gate_permission,
-    aws_lambda_permission.lambda_etl_permission,
-    aws_lambda_permission.lambda_datachecks_permission,
-    aws_lambda_permission.lambda_data_loader_permission,
-    aws_lambda_permission.lambda_file_merge_permission,
-    aws_lambda_permission.lambda_forwardflow_savefile_permission,
-    aws_lambda_permission.lambda_paper_statement_permission,
-    aws_lambda_permission.lambda_agency_posting
-  ]
+resource "aws_lambda_permission" "lambda_gate_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-skeps-gatekeeper"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.customer_id}-tops-${var.environment}-s3-dps"
 }
-# other customer triggers go here
-resource "aws_s3_bucket_notification" "skeps_gatekeeper_bucket_customers" {
-  bucket = "${var.customer_id}-tops-${var.environment}-s3-dps"
 
-  count = var.customer_id != "fnbo" ? 1 : 0
+resource "aws_lambda_permission" "lambda_data_loader_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-skeps-selective-data-loader"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.customer_id}-tops-${var.environment}-s3-dps"
+}
 
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-gatekeeper"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/${var.customer_id}/etl/original-files/"
-  }
+resource "aws_lambda_permission" "lambda_etl_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-skeps-etl"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.customer_id}-tops-${var.environment}-s3-dps"
+}
 
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-etl"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/${var.customer_id}/etl/manageable-files/etl/"
-  }
+resource "aws_lambda_permission" "lambda_datachecks_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-skeps-datachecks"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.customer_id}-tops-${var.environment}-s3-dps"
+}
 
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-datachecks"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/${var.customer_id}/etl/manageable-files/datacheck/"
-  }
+resource "aws_lambda_permission" "lambda_file_merge_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-skeps-file-merger"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.customer_id}-tops-${var.environment}-s3-dps"
+}
 
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-selective-data-loader"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/${var.customer_id}/etl/process-start/"
-  }
+resource "aws_lambda_permission" "lambda_forwardflow_savefile_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-af_forwardflow_savefile"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.customer_id}-tops-${var.environment}-s3-dps"
+}
 
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-skeps-file-merger"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/${var.customer_id}/etl/manageable-files/file-merger/"
-  }
+resource "aws_lambda_permission" "fnbo_permission" {
+  statement_id  = "AllowExecutionFromEventBridgeFnbo"
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-bureau_tagging_deferred_loans_update"
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.fnbo_rule.arn
+}
 
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-clp-ach-reconciliation-report"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/${var.customer_id}/outbound/servicing/nacha/"
-  }
+# FNBO permission
+resource "aws_lambda_permission" "allow_eventbridge_invoke_fnbo" {
+  count = var.customer_id == "fnbo" ? 1 : 0
+  statement_id  = "AllowExecutionFromEventBridgeFnbo"
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-bureau_tagging_misc_code_update"
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.bureau_tagging_misc_code_update_fnbo.arn
+}
 
-  lambda_function {
-    lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:tops-etl-${var.environment}-clp-ach-api-reconciliation-report"
-    events              = ["s3:ObjectCreated:*"]
-    filter_prefix       = "clients/${var.customer_id}/reports/ach_reconciliation_report/ps_start/"
-  }
+# AF permission
+resource "aws_lambda_permission" "allow_eventbridge_invoke_af" {
+  count = var.customer_id == "fnbo" ? 1 : 0
+  statement_id  = "AllowExecutionFromEventBridgeAf"
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-bureau_tagging_misc_code_update"
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.bureau_tagging_misc_code_update_af.arn
+}
 
-  lambda_function {
-     lambda_function_arn = "arn:aws:lambda:us-east-1:${data.aws_caller_identity.current.account_id}:function:pos-techops-serverless-${var.environment}-agencyPostingHandler"
-     events              = ["s3:ObjectCreated:*"]
-     filter_prefix       = "clients/${var.customer_id}/agencyFiles/process_start"
-     filter_suffix       = ".csv"
-  }
+# UCL permission
+resource "aws_lambda_permission" "allow_eventbridge_invoke_ucl" {
+  count = var.customer_id == "fnbo" ? 1 : 0
+  statement_id  = "AllowExecutionFromEventBridgeUcl"
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-bureau_tagging_misc_code_update"
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.bureau_tagging_misc_code_update_ucl.arn
+}
 
-  depends_on = [
-    module.deploy_lambdas,
-    aws_lambda_permission.lambda_gate_permission,
-    aws_lambda_permission.lambda_etl_permission,
-    aws_lambda_permission.lambda_datachecks_permission,
-    aws_lambda_permission.lambda_data_loader_permission,
-    aws_lambda_permission.lambda_file_merge_permission,
-    aws_lambda_permission.lambda_paper_statement_permission,
-    aws_lambda_permission.lambda_clp_ach_reconciliation_report_permission,
-    aws_lambda_permission.lambda_clp_ach_api_reconciliation_report_permission
 
-  ]
+resource "aws_lambda_permission" "af_permission" {
+  count = var.customer_id == "fnbo" ? 1 : 0
+  statement_id  = "AllowExecutionFromEventBridgeAf"
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-bureau_tagging_deferred_loans_update"
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.af_rule.arn
+}
+
+resource "aws_lambda_permission" "ucl_permission" {
+  count = var.customer_id == "fnbo" ? 1 : 0
+  statement_id  = "AllowExecutionFromEventBridgeUcl"
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-bureau_tagging_deferred_loans_update"
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.ucl_rule.arn
+}
+
+
+resource "aws_lambda_permission" "lambda_paper_statement_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-af-paper-statement"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.customer_id}-tops-${var.environment}-s3-dps"
+}
+
+resource "aws_lambda_permission" "lambda_clp_ach_reconciliation_report_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-clp-ach-reconciliation-report"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.customer_id}-tops-${var.environment}-s3-dps"
+}
+
+resource "aws_lambda_permission" "lambda_clp_ach_api_reconciliation_report_permission" {
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-clp-ach-api-reconciliation-report"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.customer_id}-tops-${var.environment}-s3-dps"
+}
+
+resource "aws_lambda_permission" "lambda_activity_file_formatter_permission" {
+  statement_id  = "AllowExecutionFromEventBridgeActivityFile"
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-activity-file-formatter"
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.activity_file_formatter_schedule.arn
+}
+
+resource "aws_lambda_permission" "lambda_activity_file_formatter_permission_sunday" {
+  statement_id  = "AllowExecutionFromEventBridgeActivityFileSunday"
+  action        = "lambda:InvokeFunction"
+  function_name = "tops-etl-${var.environment}-activity-file-formatter"
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.activity_file_formatter_schedule_sunday.arn
+}
+
+resource "aws_lambda_permission" "lambda_agency_posting" {
+  statement_id  = "AllowExecutionFromAgencyPosting"
+  action        = "lambda:InvokeFunction"
+  function_name = "pos-techops-serverless-${var.environment}-agencyPostingHandler"
+  principal     = "s3.amazonaws.com"
+  source_arn    = "arn:aws:s3:::${var.customer_id}-tops-${var.environment}-s3-dps"
 }
