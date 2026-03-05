9b9c99ca29a31922db89114c61ba8c833ae6c2f9
amitsingh011194 <astallvibes@gmail.com>
2026-03-05 16:57:56 +0530

    Update New.py

--- New.py
+++ New.py
@@ -1,28 +1,77 @@
-#!/bin/bash
+resource "aws_lambda_function" "deploy_lambda" {
+  function_name    = "${var.customer}-ssp-${var.env}-lambda-${var.lambda_name}"
+  description      = "Created in repo loan-servicing-api commit:${var.commit_id} branch:${var.branch} " 
+  role             = aws_iam_role.lambda_iam_role.arn
+  handler          = (
+    var.lambda_name == "${var.customer}-ivr-processing" || 
+    var.lambda_name == "${var.customer}-ivr-tabapay-call" ||
+    var.lambda_name == "${var.customer}-ivr-conduent-call" || 
+    var.lambda_name == "${var.customer}-campaign-activity-code" ||
+    var.lambda_name == "${var.customer}-campaign-customer-profiles" ||
+    var.lambda_name == "${var.customer}-ivr-ssp-integration" ||
+    var.lambda_name == "${var.customer}-campaign-customerdata-manupulation" ||
+    var.lambda_name == "${var.customer}-campaign-database-update" ||
+    var.lambda_name == "${var.customer}-campaign-run-status" ||
+    var.lambda_name == "${var.customer}-campaign-lambda" || 
+    var.lambda_name == "${var.customer}-campaign-profiles-manupulation"
+  ) ? "lambda_function.lambda_handler" : "index.lambda_handler"
+  runtime          = (
+    var.lambda_name == "${var.customer}-ivr-processing" || 
+    var.lambda_name == "${var.customer}-ivr-tabapay-call" ||
+    var.lambda_name == "${var.customer}-ivr-conduent-call" || 
+    var.lambda_name == "${var.customer}-campaign-activity-code" ||
+    var.lambda_name == "${var.customer}-campaign-customer-profiles" ||
+    var.lambda_name == "${var.customer}-ivr-ssp-integration" ||
+    var.lambda_name == "${var.customer}-campaign-customerdata-manupulation" ||
+    var.lambda_name == "${var.customer}-campaign-database-update" ||
+    var.lambda_name == "${var.customer}-campaign-run-status" ||
+    var.lambda_name == "${var.customer}-campaign-lambda" || 
+    var.lambda_name == "${var.customer}-campaign-profiles-manupulation"
+  ) ? "python3.10" : "nodejs20.x"
+  memory_size      = 5120
+  timeout          = 600
+  s3_bucket        = "exl-${var.customer}-${var.env}-templates"
+  s3_key           = "lambdas/${var.lambda_name}"
+  source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
-# Check if a directory path argument was provided
-if [ $# -ne 1 ]; then
-    echo "Usage: $0 <directory_path>"
-    exit 1
-fi
+  # Conditionally set layers only if the lambda is "${var.customer}-ivr-tabapay-call"
+  layers = ( 
+    var.lambda_name == "${var.customer}-campaign-customer-profiles" || 
+    var.lambda_name == "${var.customer}-ivr-tabapay-call" || 
+    var.lambda_name == "${var.customer}-ivr-conduent-call" || 
+    var.lambda_name == "${var.customer}-campaign-activity-code" || 
+    var.lambda_name == "${var.customer}-ivr-ssp-integration" ||
+    var.lambda_name == "${var.customer}-campaign-customerdata-manupulation" ||
+    var.lambda_name == "${var.customer}-campaign-database-update" ||
+    var.lambda_name == "${var.customer}-campaign-run-status" ||
+    var.lambda_name == "${var.customer}-campaign-lambda" || 
+    var.lambda_name == "${var.customer}-campaign-profiles-manupulation"
+    ) ? var.lambda_layer_arns : []
+    
+    signing_profile_version_arn = var.code_signing_profile_arn
 
-# Verify that the provided path is a directory
-directory_path="$1"
-if [ ! -d "$directory_path" ]; then
-    echo "Error: '$directory_path' is not a directory."
-    exit 1
-fi
+    dead_letter_config {
+    target_arn = var.lambda_dlq_arn
+  }
+  reserved_concurrent_executions = var.lambda_concurrency_limit
 
-# Change to the specified directory
-cd "$directory_path" || exit 1
+  vpc_config {
+    subnet_ids         = var.lambda_subnet_ids
+    security_group_ids = var.lambda_security_group_ids
+  }
+  environment {
+    variables = fileexists("${var.lambda_path}/env_vars.json") ? jsondecode(templatefile("${var.lambda_path}/env_vars.json", { 
+            ACCOUNT_ID  = var.aws_acct_id, 
+            AWS_REGION  = var.aws_region, 
+            CUSTOMER_ID = var.customer, 
+            ENVIRONMENT = var.environment 
+          })) : {}
+  }
 
-# Loop through all directories in the specified directory
-for dir in */; do
-    if [ -d "$dir" ]; then
-        echo "Running 'npm install' in $dir"
-        cd "$dir" || exit 1
-        npm install
-        cd ..
-        echo "'npm install' completed in $dir"
-    fi
-done
+  kms_key_arn = var.kms_key_id
+
+  tracing_config {
+    mode = "Active"
+  }
+  depends_on = [aws_s3_object.upload_lambda_zip]
+}
